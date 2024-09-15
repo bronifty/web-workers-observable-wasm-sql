a63576fc33e5b456f1696078ba49f169b8d52683
bronifty <bronifty@gmail.com>
2024-09-15 19:36:46 -0400

    this

--- web-workers/marcs-observable-worker/main.mjs
+++ web-workers/marcs-observable-worker/main.mjs
@@ -1,8 +1,8 @@
-import {
-  observableInput1,
-  observableInput2,
-  observableTotal,
-} from "./store.mjs";
+// import {
+//   observableInput1,
+//   observableInput2,
+//   observableTotal,
+// } from "./store.mjs";
 // Create a new web worker
 const worker = new Worker("worker.mjs", { type: "module" });
 
@@ -22,21 +22,11 @@ input2.addEventListener("change", () =>
 );
 
 worker.onmessage = function (event) {
-  const { element, value } = event.data;
-  console.log(
-    `in main thread; worker.onmessage: ${JSON.stringify(
-      { element, value },
-      null,
-      2
-    )}`
-  );
-  if (element === "input1") {
-    observableInput1.value = value;
-  } else if (element === "input2") {
-    observableInput2.value = value;
-  }
+  // const { element, value } = event.data;
+  console.log(`in main thread; worker.onmessage: ${event.data}`);
+  total.textContent = event.data;
 };
 
-observableTotal.subscribe((value) => {
-  total.textContent = value;
-});
+// observableTotal.subscribe((value) => {
+//   total.textContent = value;
+// });
